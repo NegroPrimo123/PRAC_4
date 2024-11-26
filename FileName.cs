acb5385a2d28a57d2bf044932240fda1d870b1d4
NegroPrimo123 <pereverzevm44@gmail.com>
2024-11-26 09:55:52 +0300

    Add files via upload

---
+++ FileName.cs
@@ -0,0 +1,17 @@
+﻿public class User
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int Age { get; set; }
+
+    public User(string firstname, string username, string password, int age)
+    {
+        Firstname = firstname;
+        Username = username;
+        Password = password;
+        Age = age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Data.SqlClient; 
+
+class Program
+{
+    static string connectionString = "Server=your_server;Database=your_database;User  Id=your_user;Password=your_password;";
+
+    static void Main(string[] args)
+    {
+        ShowAllUsers();
+    }
+
+    static void ShowAllUsers()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Users", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = reader.GetInt32(0);
+                    string name = reader.GetString(1);
+                    Console.WriteLine($"Id: {id}, Name: {name}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+    }
+}
